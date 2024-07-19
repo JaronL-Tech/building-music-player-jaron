30d2de1268b7a16cf5f712f02409255658a608a7
Jaron Leach <jaronleach98@gmail.com>
2024-07-19 01:25:35 -0400

    started and finished my sortSong function which used the user data to sort and order the songs in a array.

--- script.js
+++ script.js
@@ -103,5 +103,20 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
-const sortSongs = () => {};
+const sortSongs = () => {
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
 renderSongs(userData?.songs);
