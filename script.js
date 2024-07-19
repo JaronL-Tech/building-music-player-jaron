aec24e454ffdeee9cd7aedead2fd345c20410462
Jaron Leach <jaronleach98@gmail.com>
2024-07-19 11:58:46 -0400

    created a previous song function that will go back to the last song played.

--- script.js
+++ script.js
@@ -115,6 +115,16 @@ const playNextSong = () => {
   }
 };
 
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+  }
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -149,6 +159,8 @@ playButton.addEventListener("click", () => {
   }
 });
 pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
